1c4d54dd23e8a604a27ee0b02639efdbec1d8897
dk <dayka6601@gmail.com>
2025-01-07 11:24:46 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <div className='h-screen flex flex-col justify-center items-center bg-black/95'> 
       <h1 className='text-5xl text-center text-transparent p-10 font-bold bg-gradient-to-br from-fuchsia-500 to-blue-600 bg-clip-text'>Ne vous souciez plus des chiffres, grâce à <br /> Kalc.</h1> 
       <a href="" className='border border-blue-500 text-lg p-3 rounded-lg text-white'>Calculer</a>
+      <p>
+        
+      </p>
     </div>
   )
 }
