963d05ce765d5764d01a963d0107f300ab8a86ba
dk <dayka6601@gmail.com>
2025-01-07 11:24:15 +0100

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export function Header () {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            Calculez
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
