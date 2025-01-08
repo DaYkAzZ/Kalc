74c94614331f8cad600d2912548e24f090b437d1
dk <dayka6601@gmail.com>
2025-01-08 13:38:53 +0100

    remove Header component and update Calc functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { Header } from './components/Header';
-import { Calc } from './components/utils/calc';
+import { Calc } from './components/utils/Calc';
 
 function App() {
 
@@ -9,7 +8,6 @@ function App() {
 
   return (
     <div className='bg-black/95'>
-      <Header />
       <div className='h-screen flex flex-col justify-center items-center'>
         <h1 className='text-6xl text-center text-white p-10 font-bold'>Ne vous souciez plus des <span className='bg-gradient-to-tr from-fuchsia-500 to-blue-600 bg-clip-text text-transparent'>chiffres</span>, grâce à <br />
           <span className='bg-gradient-to-br from-fuchsia-500 to-blue-600 bg-clip-text text-transparent'>Kalc.</span></h1>
@@ -21,7 +19,7 @@ function App() {
           }
         }} style={{
           borderColor: calc ? 'red' : 'rgb(59, 130, 246)',
-        }} className='border-2 border-blue-500 text-lg mt-5 p-3 rounded-xl text-white'>
+        }} className={`border-2 border-blue-500 text-lg mt-5 p-3 rounded-xl text-white`}>
           {calc ? 'Fermer' : 'Calculer'}
         </button>
         <div className={`transition-all duration-1000 ${calc ? 'opacity-100' : 'opacity-0'} ${calc ? 'scale-100' : 'scale-75'} ${calc ? 'p-16' : 'p-0'}`}>
--- src/components/utils/Calc.jsx
+++ src/components/utils/Calc.jsx
@@ -4,15 +4,28 @@ import React, { useState } from 'react';
 
 export function Calc() {
     const [workers, setWorkers] = useState([]);
-
     const newWorker = (e) => {
         e.preventDefault();
         setWorkers([...workers, ...tjmData]);
     };
 
+    function chooseTjm() {
+        const [tjm, setTjm] = useState(null);
+        workers.forEach(worker => {
+            if(worker.experienceLevels === 'Junior') {
+                setTjm(worker.juniorTJM);
+            } else if(worker.experienceLevels === 'Senior') {
+                setTjm(worker.seniorTJM);
+            }
+        });
+    }
+
+    chooseTjm()
+
+
     return (
         <div className="w-[900px] rounded-lg shadow-md flex flex-col p-10 text-black bg-white">
-            <h1 className="font-bold text-4xl mb-5 bg-gradient-to-l from-fuchsia-500 to-blue-600 bg-clip-text text-transparent">
+            <h1 className="font-bold text-4xl py-2 mb-5 bg-gradient-to-l from-fuchsia-500 to-blue-600 bg-clip-text text-transparent">
                 Chiffrer votre projet
             </h1>
             <form className="flex flex-col">
@@ -23,33 +36,35 @@ export function Calc() {
                     <hr />
                     <div className='my-3 flex flex-col'>
                         <h3 className='font-bold my-3'>
-                        Ancienneté / Métier / TJM :
+                            Ancienneté / Métier / TJM :
                         </h3>
-                        <div className='flex flex-row justify-between items-center'>
-                            <div>
-                                <select className='p-2 rounded-lg shadow-lg w-full'>
-                                    <option value="junior">Junior</option>
-                                    <option value="senior">Senior</option>
-                                </select>
+                            <div className='flex flex-row justify-between items-center'>
+                                <div>
+                                    <select className='p-2 rounded-lg shadow-lg w-full'>
+                                        {workers.map((worker, index) => (
+                                            <option key={index} value={worker.experienceLevels}>{worker.experienceLevels[index]}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div>
+                                    <select className='p-2 rounded-lg shadow-lg w-full'>
+                                        {workers.map((worker, index) => (
+                                            <option key={index} value={worker.role}>{worker.role}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className='p-2 rounded-lg shadow-lg'>
+                                    {/* pas bon, tjm en double */}
+                                    <select className='w-full'>
+                                        {workers.map((worker, index) => (
+                                            <option key={index}> {worker.juniorTJM} </option>
+                                        ))}
+                                    </select>
+                                </div>
                             </div>
-                            <div>
-                                <select className='p-2 rounded-lg shadow-lg w-full'>
-                                    {workers.map((worker) => (
-                                        <option value={worker.role}>{worker.role}</option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div>
-                                <select className='p-2 rounded-lg shadow-lg w-full'>
-                                    {workers.map((worker) => (
-                                        <option value={worker.juniorTJM }>{worker.juniorTJM} €/J</option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
                     </div>
                     <button onClick={newWorker} className='bg-blue-500 text-white p-2 rounded-lg mt-2'>
-                        <i class="ri-add-line"></i> Nouv.
+                        <i className="ri-add-line"></i> Nouv.
                     </button>
                 </div>
             </form>
